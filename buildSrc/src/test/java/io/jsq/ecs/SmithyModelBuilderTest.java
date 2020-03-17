f5cd7907e28b94ab3c46d891f6259c9f4f0a7949
Jonathan Eskew <jonathan@jeskew.net>
2020-03-17 11:58:40 -0400

    Update model builder to use new array normalization flag instead of a whitelist of known list elements

--- buildSrc/src/test/java/io/jsq/ecs/SmithyModelBuilderTest.java
+++ buildSrc/src/test/java/io/jsq/ecs/SmithyModelBuilderTest.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeIndex;
 import software.amazon.smithy.model.validation.ValidatedResult;
 
 class SmithyModelBuilderTest {
@@ -17,5 +21,18 @@ class SmithyModelBuilderTest {
 
         Assertions.assertFalse(result.isBroken());
         Assertions.assertTrue(result.getResult().isPresent());
+    }
+
+    @Test
+    void testModelBuilderCorrectlyAppliesArrayNormalization() {
+        List<Schema> schemata = Loader.loadSchemata();
+        SmithyModelBuilder builder = new SmithyModelBuilder("example.test", "Record");
+        schemata.forEach(builder::addSchema);
+        ShapeIndex index = builder.build().unwrap().getShapeIndex();
+
+        Shape rootShape = index.getShape(ShapeId.from("example.test#Record")).get();
+        MemberShape tagsMember = rootShape.asStructureShape().flatMap(ss -> ss.getMember("tags")).get();
+        Shape tagsTarget = index.getShape(tagsMember.getTarget()).get();
+        Assertions.assertTrue(tagsTarget.isListShape());
     }
 }
